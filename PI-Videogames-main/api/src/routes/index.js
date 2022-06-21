4e7dd55a59ace0c5ab53e0d333f6b2366b23bd1e
zSarcoz <cesar.cottua@gmail.com>
2022-06-21 19:06:04 -0400

    route 1 finish

--- PI-Videogames-main/api/src/routes/index.js
+++ PI-Videogames-main/api/src/routes/index.js
@@ -10,68 +10,67 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-const getGames = async () => {
+const getGames = async()=>{
   try {
-    const url_game = axios.get(`https://api.rawg.io/api/games?key=${APIKEY}`);
-    const api_Game = await url_game.data?.results.map((game) => {
-      return {
-        id: game.id,
-        name: game.name,
-        description: game.description,
-        rating: game.rating,
-        genres: game.genres.map((genre) => genre.name),
-        platforms: game.platforms.map((el) => el.platform.name),
-        release_date: game.platforms.map((el) => el.released_at),
-      };
-    });
-    return api_Game;
-  } catch (e) {
-    return new Error(error + "error en el servidor");
+      
+      const url_game = await axios.get(`https://api.rawg.io/api/games?key=${APIKEY}`);
+      const apiGame = await url_game.data?.results.map(game => {
+          return {
+              id:game.id,
+              name:game.name,
+              rating:game.rating,
+              genres:game.genres.map(genre => genre.name),
+              platforms:game.platforms.map(game => game.platform.name),
+              release_date:game.platforms.map(game => game.released_at)
+              
+          }
+      })
+      return apiGame;
+  } catch (error) {
+      return new Error(error + 'error en el servidor')
   }
-};
-
-const getByDb = async()=>{
-    
-        //SELECT * FROM videogames
-        return await Videogame.findAll({
-            include:{
-                model:Genre,
-                attributes:['name'],
-                through:{
-                    attributes:[]
-                }
-            }
-        })
-} 
-
-// const getDataApi = async(req,res)=>{
-//     const api_Game = await getGames();
-//     return api_Game;
-// }  XXXX
-
-const getAllGames = async () => {
-    try{
-        const api_Game = await getGames();
-        const myDb_info = await getByDb();
-        const allInfo = api_Game.concat(myDb_info);
-        return allInfo;
-    }catch(err){
-        return new Error(err + "error en el servidor");
-    }
 }
 
-router.get("/", async (req, res) => {
-    try{
-        const {name} = req.query;
-        const allGames = await getAllGames();
-        if(name){
-            const games = allGames.filter(game=>game.name.toLowerCase().includes(name.toLowerCase()));
-            return res.status(200).json(games);
-        }
-    }catch(error){
-        return res.status(400).json({error:'error en el servidor'});
-    }
+// SELECT * FROM videogames
+const getByDb = async() => {
+  return await Videogame.findAll({
+      include:{
+          model:Genre,
+          attributes:['name'],
+          through:{
+              attributes:[],
+          }
+      }
+  })
+}
 
-})
+const getAllGames = async()=>{
+  try {
+      const gamesInfo = await getGames();
+      const dbInfo = await getByDb();
+      const infoAll = gamesInfo.concat(dbInfo);
+      return infoAll;  
+  } catch (error) {
+      return new Error(error +'error en el servidor')
+  }
+}
+router.get('/videogames', async(req, res) =>{
+  try {
+    const {name} = req.query;
+    const games = await getAllGames();
+    if(name){
+        let gameName = games.filter(game => game.name.toLowerCase().includes(name.toLowerCase()));
+        gameName.length > 0
+            ?res.status(200).json(gameName)
+            :res.status(404).send('No se encontro un video juego con ese nombre')
+     
+
+    }else{
+        res.status(200).json(games)
+    }   
+} catch (error) {
+    res.status(404).json({error:'error en el servidor'})
+}
+});
 
 module.exports = router;
