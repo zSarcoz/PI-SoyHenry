5d373e249004c26c52fa76a550cb2d9cd15bf8ed
zSarcoz <cesar.cottua@gmail.com>
2022-06-21 14:12:08 -0400

    models finish

--- PI-Videogames-main/api/src/models/Videogame.js
+++ PI-Videogames-main/api/src/models/Videogame.js
@@ -4,9 +4,45 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    description:{
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    release_date:{
+      type: DataTypes.DATE,
+    },
+    rating:{
+      type: DataTypes.INTEGER,
+    },
+    platforms:{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },{timestamp: false});
+
+  sequelize.define('videogame_genre', {
+    id: {
+
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
+  })
+  
 };
